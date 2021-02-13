0e45a58b716d99aa2ea847efdd9cc035b37aa979
amrik <73204325+amrikverma@users.noreply.github.com>
2021-02-12 22:40:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function search_covid() { 
     let input = document.getElementById('searchbar').value 
     input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('animals'); 
+    let x = document.getElementsByClassName('covid-options'); 
       
     for (i = 0; i < x.length; i++) {  
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
